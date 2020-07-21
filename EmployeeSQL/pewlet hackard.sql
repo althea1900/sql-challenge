aad26b8f0179ca2140fb48aadc0beaf6cc8bc6f9
Althea McMillian <amcmilli@richmond.edu>
2020-07-21 00:04:53 -0400

    one last update to queries

--- EmployeeSQL/pewlet hackard.sql
+++ EmployeeSQL/pewlet hackard.sql
@@ -32,7 +32,7 @@ order by e.emp_no
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" 
 --    and last names begin with "B."
-SELECT emp_no, first_name,  last_name,  sex
+SELECT first_name,  last_name,  sex
 FROM employees  
 WHERE first_name = 'Hercules' and last_name Like 'B%'
 ORder by last_name;

6919e172ba52ce1309000fab4ae473a1009a45fe
Althea McMillian <amcmilli@richmond.edu>
2020-07-21 00:09:12 -0400

    another update

--- EmployeeSQL/create tables.sql
+++ EmployeeSQL/create tables.sql
@@ -1,7 +1,3 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/cdTFnN
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
 
 CREATE TABLE Employees (
     emp_no INT   NOT NULL,
